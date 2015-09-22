026cbd101d36756fcf72caf9828045c016d00a28
Klaus Villaca <klausvillaca@gmail.com>
2015-09-21 21:55:29 -0400

    Initial Commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Klaus Villaca on 9/21/15.
+//  Copyright © 2015 Klaus Villaca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
